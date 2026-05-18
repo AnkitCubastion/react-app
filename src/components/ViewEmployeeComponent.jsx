f340624b1e95f70017f1d65fce9f6f80028f1b9f
AnkitCubastion <ankit.prasad@cubastion.com>
2026-05-18 10:51:23 +0530

    Add greeting header to ViewEmployeeComponent

--- src/components/ViewEmployeeComponent.jsx
+++ src/components/ViewEmployeeComponent.jsx
@@ -23,6 +23,7 @@ class ViewEmployeeComponent extends Component {
             <div>
                 <br></br>
                 <div className = "card col-md-6 offset-md-3">
+                    <h1>Hello</h1>
                     <h3 className = "text-center"> View Employee Details</h3>
                     <div className = "card-body">
                         <div className = "row">
