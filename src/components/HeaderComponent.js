a62d6134f5062bca57755303a536aea40c455e5d
AnkitCubastion <ankit.prasad@cubastion.com>
2026-05-18 13:47:21 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -14,7 +14,7 @@ class HeaderComponent extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="https://javaguides.net" className="navbar-brand">Employee Management App Updated Cubastion</a></div>
+                    <div><a href="https://javaguides.net" className="navbar-brand">Employee Management App Updated Cubastion update for testing</a></div>
                     </nav>
                 </header>
             </div>
